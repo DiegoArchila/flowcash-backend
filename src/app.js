6e245847ca173f692caf2a03cd9b8c78ba3596d9
Diego Alonso Archila Torres <daat3523@gmail.com>
2025-07-19 11:28:02 -0500

    [UPDATE] Updated the .env environment to load the . env added ENV_CORS

--- src/app.js
+++ src/app.js
@@ -46,10 +46,12 @@ app.use(express.static("public"));
 if (env === 'development') {
 
   console.log('Running in development mode');
+
+  const allowedOrigins = process.env.APP_CORS ? process.env.APP_CORS_DEV.split(',') : [];
   
   app.use(cors({
 
-    origin: '*', // Allow all origins in development
+    origin: process.env.APP_CORS ? process.env.APP_CORS_DEV.split(',') : [], // Allow all origins in development
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
@@ -61,9 +63,7 @@ if (env === 'development') {
   console.log('Running in production mode');
 
   // Origins allowed in production
-  const allowedOrigins = [
-    'https://www.mabla.app', // Production domain
-  ];
+  const allowedOrigins = process.env.APP_CORS ? process.env.APP_CORS.split(',') : [];
 
   app.use(cors({
 
