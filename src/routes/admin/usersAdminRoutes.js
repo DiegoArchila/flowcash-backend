736a3e0ffeac0478744a66d43be434ad8b289901
Diego Alonso Archila Torres <daat3523@gmail.com>
2025-07-21 21:58:05 -0500

    [Fixed] Fixed the file UsersAdminRoutes.js the return routes for collection must set firt

--- src/routes/admin/usersAdminRoutes.js
+++ src/routes/admin/usersAdminRoutes.js
@@ -7,24 +7,24 @@ const userAdminController = require("../../controllers/admin/users/usersAdminCon
 
 router
     // [USERS]
+    .get("/api/flowcash/admin/users", userAdminController.getAlls)
     .post("/api/flowcash/admin/users/create", userAdminController.create)
     .post("/api/flowcash/admin/users/:id/update", userAdminController.update)
     .post("/api/flowcash/admin/users/:id/update/password", userAdminController.updatePassword)
     .get("/api/flowcash/admin/users/:id", userAdminController.findById)
-    .get("/api/flowcash/admin/users", userAdminController.getAlls)
 
     // [ROLES]
+    .get("/api/flowcash/admin/users/roles", rolesAdminController.getAlls)
     .post("/api/flowcash/admin/users/roles/create", rolesAdminController.create)
     .post("/api/flowcash/admin/users/roles/:id/update", rolesAdminController.update)
     .delete("/api/flowcash/admin/users/roles/:id/delete", rolesAdminController.delete)
     .get("/api/flowcash/admin/users/roles/:id", rolesAdminController.findById)
-    .get("/api/flowcash/admin/users/roles", rolesAdminController.getAlls)
 
     // [DNITYPES]
+    .get("/api/flowcash/admin/users/dnitypes", dnitypeAdminController.getAlls)
     .post("/api/flowcash/admin/users/dnitypes/create", dnitypeAdminController.create)
     .post("/api/flowcash/admin/users/dnitypes/:id/update", dnitypeAdminController.update)
     .delete("/api/flowcash/admin/users/dnitypes/:id/delete", dnitypeAdminController.delete)
     .get("/api/flowcash/admin/users/dnitypes/:id", dnitypeAdminController.findById)
-    .get("/api/flowcash/admin/users/dnitypes", dnitypeAdminController.getAlls)
 
 module.exports=router;
